457c762fe9cd3eefeced2ca87bdbbe4f6750599a
vanos2k <49749119+vanos2k@users.noreply.github.com>
2020-08-31 22:45:32 +0300

    finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './scss/index.scss';
+import {Excel} from '@/components/excel/Excel';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
+import {Header} from '@/components/header/Header';
 
-console.log('working');
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+
+excel.render();
