b1848d540139251a44229fab84f750e7bfdaf486
vanos2k <49749119+vanos2k@users.noreply.github.com>
2020-09-01 20:57:43 +0300

    resize finish

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,52 +3,62 @@ const CODES = {
   Z: 90,
 };
 
-function createCell() {
+function createCell(_, index) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" 
+         data-col="${index}" 
+         contenteditable
+    >
+    </div>
   `;
 }
 
-function createColumn(col) {
+function createColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+        ${col}
+        <div class="col-resize" data-resize="col"></div>
+    </div>
   `;
 }
 
 function createRow(content, index = '') {
+  const resizer = index
+    ? '<div class="row-resize" data-resize="row"></div>'
+    : ''
+  ;
   return `
-    <div class="row">
-        <div class="row-info">${index}</div>
+    <div class="row" data-type="resizable">
+        <div class="row-info">
+            ${index}
+            ${resizer}
+        </div>
         <div class="row-data">${content}</div>
     </div>
   `;
 }
 
-function toChar(index) {
+function toChar(_, index) {
   return String.fromCharCode(CODES.A + index);
 }
-
-// function cellContent(rowIndex, collIndex) {
-//   return `${toChar(collIndex)}${rowIndex}`;
-// }
 
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
   const cols = new Array(colsCount)
       .fill('')
-      .map((el, index) => toChar(index))
-      .map(el => createColumn(el))
+      .map(toChar)
+      .map(createColumn)
       .join('');
 
   rows.push(createRow(cols));
 
-  for (let i = 0; i < rowsCount; i++) {
+  for (let i = 1; i < rowsCount + 1; i++) {
     const cells = new Array(colsCount)
         .fill('')
-        .map(cell => createCell())
+        .map(createCell)
         .join('');
-    rows.push(createRow(cells, i + 1));
+    rows.push(createRow(cells, i));
   }
 
   return rows.join('');
